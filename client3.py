e12848c86b825dcd6bda0b407980018bd0e28016
surendar_p <surendar.p2021@vitstudent.ac.in>
2023-06-23 18:40:32 +0530

    Create Patch File

--- client3.py
+++ client3.py
@@ -31,18 +31,17 @@ N = 500
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2  # Calculate the average price
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    ratio = price_a / price_b
+    return ratio
 
 
 # Main
@@ -51,9 +50,11 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            ratio = getRatio(bid_price, ask_price)  # Calculate the ratio using bid_price and ask_price
+            print("Ratio %s" % ratio)
 
-        print("Ratio %s" % getRatio(price, price))
+
+
